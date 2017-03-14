648eb61abcaa047edac676be85d16069d7b0eff9
ilpaijin <ilpaijin@gmail.com>
2017-03-14 18:13:37 +0100

    addded message and validation

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -3,6 +3,9 @@
 namespace Ilpaijin\Controller;
 
 use Exception;
+use Ilpaijin\Exception\UnprocessableEntityException;
+use MessageBird\Objects\Message as BirdMessage;
+use Ilpaijin\Validator\Constraints\MessageConstraint as MessageConstraint;
 
 /**
  * Class MessageController
@@ -15,13 +18,28 @@ class MessageController extends ControllerDIAware
      */
     public function getAll()
     {
+        $messageService = $this->container->get('messagebird');
+        $validator = $this->container->get('validator');
+
         try{
-            $balance = $this->container->get('messagebird')->balance->read();
+            $message             = new BirdMessage();
+            $message->originator = 'MessageBird';
+            $message->recipients = array('0034684125308');
+            $message->body       = 'LØrem aeconsectetur adipiscing elit. Morbi a commodo est. Curabitur imperdiet lacinia tristique. Orci varius natoque penatibus et magnis dis parturient montes, nascetur massa nunc.';
+            $message->datacoding = 'unicode';
+//            $messageResult = $messageService->messages->create($message);
         } catch (Exception $e) {
             return ['error' => $e->getMessage()];
         }
 
-        return $balance->amount;
+        if (!$validator->validate($message, new MessageConstraint)) {
+            throw new UnprocessableEntityException();
+        }
+
+        var_dump(mb_strlen($message->body, '8bit'));
+        exit;
+
+//        return ['data' => ['service-result' => $messageResult]];
     }
 
     public function post() {
