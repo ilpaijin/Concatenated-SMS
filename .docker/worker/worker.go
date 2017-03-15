b268886f19f049296aa4292a86e9d1ab72a02878
ilpaijin <ilpaijin@gmail.com>
2017-03-15 13:15:30 +0100

    introduced messagebird sdk

--- .docker/worker/worker.go
+++ .docker/worker/worker.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"github.com/garyburd/redigo/redis"
 	"log"
 	"time"
+	"github.com/messagebird/go-rest-api"
+	"encoding/json"
 )
+
+
 
 func main()  {
 	redisConn, err := redis.Dial("tcp", "redis:6379")
@@ -15,17 +18,48 @@ func main()  {
 
 	defer redisConn.Close()
 
+	msgBirdClient := messagebird.New("clBHUTYfRaDwHdJl6yy3npYf7")
+
 	for {
 		time.Sleep(1 * time.Second)
-		showList(redisConn)
+
+		message, err := getMessageFromQueue(redisConn);
+		if err != nil {
+			log.Println(err)
+		}
+
+		if (len(message) == 0) {
+			log.Printf("empty message %+v", message)
+			continue
+		}
+
+		var queuedMsg messagebird.Message
+
+		json.Unmarshal(message, &queuedMsg)
+
+		params := &messagebird.MessageParams{
+			Type: queuedMsg.Type,
+			TypeDetails: queuedMsg.TypeDetails,
+		}
+
+		log.Printf("%+v", queuedMsg)
+
+		messageReceipt, err := msgBirdClient.NewMessage(queuedMsg.Originator, []string{"0034684125308"}, queuedMsg.Body, params)
+		if (err != nil) {
+			log.Fatalf("error: %s", err)
+		}
+
+		log.Fatal(messageReceipt)
+
+
 	}
 }
 
-func showList(redisConn redis.Conn) {
-	list, err := redis.String(redisConn.Do("RPOP", "ilpaiijn:api:concatented-sms"))
+func getMessageFromQueue(redisConn redis.Conn) ([]byte, error) {
+	message, err := redis.Bytes(redisConn.Do("RPOP", "ilpaiijn:api:concatented-sms"))
 	if err != nil {
-		log.Println(err)
+		return []byte{}, err
 	}
 
-	fmt.Println(list)
+	return message, nil
 }
