b3319bdd3508837d2d12f7ccce42a9d145b5d889
ilpaijin <ilpaijin@gmail.com>
2017-03-16 11:10:18 +0100

    removed unused

--- app/app.php
+++ app/app.php
@@ -7,7 +7,6 @@ $app = new Application(new \Ilpaijin\DIContainer());
 $app->routes['messages'] = 'MessageController';
 $app->routes['balances'] = 'BalanceController';
 
-//$app->container->set('messagebird', new MessageBird\Client("clBHUTYfRaDwHdJl6yy3npYf7"));
 $app->container->set('validator', new Ilpaijin\Validator\Validator);
 $app->container->set('queue', new Ilpaijin\Service\Queue);
 
